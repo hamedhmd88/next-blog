48ddbcbea0f823790d40b62ec496a98edf474749
Hamed Mahjoobi <hmdhamed88@gmail.com>
2025-04-01 13:12:35 +0330

    update

--- src/app/blogs/(PostList)/layout.jsx
+++ src/app/blogs/(PostList)/layout.jsx
@@ -17,6 +17,7 @@ function Layout({ children }) {
         <h1 className=" text-lg font-bold ">لیست بلاگ ها</h1>
         <Search />
       </div>
+
       <div className="grid grid-cols-12 gap-8">
         <div className="col-span-12 lg:col-span-4 xl:col-span-3 text-secondary-500 space-y-4">
           <Suspense fallback={<Spinner />}>
